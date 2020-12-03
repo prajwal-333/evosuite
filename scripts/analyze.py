47e4ebc72e671874ec1d90151c02059cb919f4b0
joelsunny <joelsunny.ch@gmail.com>
2020-12-03 19:32:23 +0530

    changes in analyze

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -4,11 +4,11 @@ import os
 
 def filter(in_file):
     data = pd.read_csv(in_file)
-    mean_val=data["mean_score_of_loaded_classes"]
-    median_val=data["median_score_of_loaded_classes"]
-    print(f"mean = {mean_val},  median = {median_val}")
+    mean_val=data.iloc[0]["mean_score_of_loaded_classes"]
+    median_val=data.iloc[0]["median_score_of_loaded_classes"]
+    print("mean = " + str(mean_val) + ",  median = " + str(median_val))
     
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
-    filter(in_file, metric_name, path_name)
+    filter(in_file)
